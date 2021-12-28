ea44009603ba5cf59f2aac7aa8eebf206c1669d8
fumiama <s862105088@gmail.com>
2021-12-28 21:52:45 +0800

    fix linux

--- lower/tun_linux.go
+++ lower/tun_linux.go
@@ -4,15 +4,15 @@
 package lower
 
 func (n *NIC) prepare() {
-	execute("/sbin/ip", "link", "set", "dev", ifcename, "mtu", "1500")
-	execute("/sbin/ip", "addr", "add", ip, "dev", ifcename)
-	execute("/sbin/ip", "route", "add", subnet, "dev", ifcename)
+	execute("/sbin/ip", "link", "set", "dev", n.ifce.Name(), "mtu", "1500")
+	execute("/sbin/ip", "addr", "add", n.ip, "dev", n.ifce.Name())
+	execute("/sbin/ip", "route", "add", n.subnet, "dev", n.ifce.Name())
 }
 
 func (n *NIC) Up() {
-	execute("/sbin/ip", "link", "set", "dev", ifcename, "up")
+	execute("/sbin/ip", "link", "set", "dev", n.ifce.Name(), "up")
 }
 
 func (n *NIC) Down() {
-	execute("/sbin/ip", "link", "set", "dev", ifcename, "down")
+	execute("/sbin/ip", "link", "set", "dev", n.ifce.Name(), "down")
 }

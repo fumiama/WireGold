302a419136d4306527f145385f5c1f47df6321fa
fumiama <s862105088@gmail.com>
2021-12-31 17:18:47 +0800

    rm tic

--- lower/nic.go
+++ lower/nic.go
@@ -5,7 +5,6 @@ import (
 	"os"
 	"os/exec"
 	"strconv"
-	"time"
 
 	"github.com/fumiama/water"
 	"github.com/fumiama/water/waterutil"
@@ -63,11 +62,7 @@ func (nc *NIC) Start(m *link.Me) {
 	buf2 := make([]byte, m.MTU()+68) // 增加报头长度与 TEA 冗余
 	off := 0
 	isrev := false
-	t := time.NewTicker(time.Millisecond)
-	for range t.C { // 从 NIC 发送
-		if !nc.hasstart {
-			break
-		}
+	for nc.hasstart { // 从 NIC 发送
 		var packet []byte
 		if off > 0 && !isrev {
 			packet = buf2

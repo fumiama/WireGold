eb49d35f654835e8f24be979f48530046abaf40b
源文雨 <41315874+fumiama@users.noreply.github.com>
2024-07-18 16:05:18 +0900

    fix(udplite): nil raddr

--- gold/p2p/udplite/lite.go
+++ gold/p2p/udplite/lite.go
@@ -48,9 +48,7 @@ func (sl *sysListener) listenUDP(ctx context.Context, laddr *net.UDPAddr) (*net.
 	}
 	sockladdr := sockaddrinterfaceinstance
 	*(**net.UDPAddr)(unsafe.Add(unsafe.Pointer(&sockladdr), unsafe.Sizeof(uintptr(0)))) = laddr
-	sockraddr := sockaddrinterfaceinstance //nolint: ineffassign
-	*(**net.UDPAddr)(unsafe.Add(unsafe.Pointer(&sockraddr), unsafe.Sizeof(uintptr(0)))) = nil
-	fd, err := internetSocket(ctx, sl.network, sockladdr, sockraddr, syscall.SOCK_DGRAM, IPPROTO_UDPLITE, "listen", ctrlCtxFn)
+	fd, err := internetSocket(ctx, sl.network, sockladdr, nil, syscall.SOCK_DGRAM, IPPROTO_UDPLITE, "listen", ctrlCtxFn)
 	if err != nil {
 		return nil, err
 	}

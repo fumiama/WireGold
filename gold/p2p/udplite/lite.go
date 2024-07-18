0c3f9111f503e77c64d0141b2de6a2318d19b94d
源文雨 <41315874+fumiama@users.noreply.github.com>
2024-07-18 15:55:58 +0900

    chore: make lint happy

--- gold/p2p/udplite/lite.go
+++ gold/p2p/udplite/lite.go
@@ -1,4 +1,4 @@
-//go:build !darwin
+//go:build !darwin && !windows
 
 package udplite
 
@@ -49,7 +49,7 @@ func (sl *sysListener) listenUDP(ctx context.Context, laddr *net.UDPAddr) (*net.
 	sockladdr := sockaddrinterfaceinstance
 	*(**net.UDPAddr)(unsafe.Add(unsafe.Pointer(&sockladdr), unsafe.Sizeof(uintptr(0)))) = laddr
 	sockraddr := sockaddrinterfaceinstance
-	sockraddr = nil
+	*(**net.UDPAddr)(unsafe.Add(unsafe.Pointer(&sockraddr), unsafe.Sizeof(uintptr(0)))) = nil
 	fd, err := internetSocket(ctx, sl.network, sockladdr, sockraddr, syscall.SOCK_DGRAM, IPPROTO_UDPLITE, "listen", ctrlCtxFn)
 	if err != nil {
 		return nil, err
